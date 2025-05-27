ab81ebd2cb8f7e0169a186910b2af774e70da638
Shashi <kodandashashidhar@gmail.com>
2025-05-28 04:01:20 +0530

    this is my second vasriable script

---
+++ specialvariable-$0-$@.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+x=$1
+y=$2
+
+echo "x is $x"
+echo "y is $y"
+echo "x + y = $(($x+$y))"
+
+echo "The number of args entered is: $#"
+echo "All entered are: $@"
+echo "The script name is: $0"

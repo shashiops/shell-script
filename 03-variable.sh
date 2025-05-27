a85e6bb922065ac31a4b1051e5e2259f7b486852
Shashi <kodandashashidhar@gmail.com>
2025-05-28 01:25:30 +0530

    this is my second vasriable script

---
+++ 03-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "enter your username"
+read -s USERNAME 
+echo "the username is: $USERNAME"
+echo "enter your password"
+read -s PASSWORD
+echo "the password is: $PASSWORD"
+
+
+
+
+

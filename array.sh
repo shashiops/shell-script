f5c7dc1ee5bf458b0eeb272f250de7e751ebeafd
Shashi <kodandashashidhar@gmail.com>
2025-05-28 01:48:05 +0530

    this is my second vasriable script

---
+++ array.sh
@@ -0,0 +1,6 @@
+FRUITS=("Apple" "Banana" "Mango")
+
+echo "The first fruit is: ${FRUITS[0]}"
+echo "The second fruit is: ${FRUITS[1]}"
+echo "The third fruit is: ${FRUITS[2]}"
+echo "All fruits are: ${FRUITS[@]}"
